1d0b47cdfa8c5e86308b4ca7ca31c7bd6a634f37
Yoni Krichevsky <yoni.k81@gmail.com>
2020-05-25 22:53:59 +0300

    Removing ObjectId use

--- db/data_layer.py
+++ db/data_layer.py
@@ -1,13 +1,11 @@
 import pymongo
-from bson import ObjectId
-
 import os
 
 
 class DataLayer:
 
     def get_year_of_birth_by_id(self, user_id):
-        user_dict = self.__db.users.find_one({"_id": ObjectId(user_id)})
+        user_dict = self.__db.users.find_one({"user_id": user_id})
         return user_dict['year_of_birth']
 
     def __init__(self):
