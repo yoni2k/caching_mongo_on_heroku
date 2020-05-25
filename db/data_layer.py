2ff5b7b94368c38d5c82dd204c96f23d22c6d5e3
Yoni Krichevsky <yoni.k81@gmail.com>
2020-05-25 23:11:51 +0300

    Changed to work according to the documentation in https://github.com/mongolab/mongodb-driver-examples/blob/master/python/pymongo_simple_example.py

--- db/data_layer.py
+++ db/data_layer.py
@@ -5,21 +5,22 @@ import os
 class DataLayer:
 
     def get_year_of_birth_by_id(self, user_id):
-        user_dict = self.__db.users.find_one({"user_id": user_id})
+        user_dict = self.__dob.find_one({"user_id": user_id})
         return user_dict['year_of_birth']
 
     def populate_db(self):
         for i in range(10000):
             dob_info = {'id': i, 'year_of_birth': 1950}
-            self.__db.users.insert_one(dob_info)
+            self.__dob.insert_one(dob_info)
 
     def __init__(self):
         mongo_uri = os.environ.get('MONGODB_URI')
 
         if mongo_uri:
-            self.__client = pymongo.MongoClient(mongo_uri)
+            client = pymongo.MongoClient(mongo_uri)
         else:
             # running locally, not on Heroku (or MongoDB was not added as AddOn)
-            self.__client = pymongo.MongoClient(mongo_uri, 27017)
+            client = pymongo.MongoClient(mongo_uri, 27017)
 
-        self.__db = self.__client["years_of_birth"]
+        db = client.get_default_database()
+        self.__dob = self.__client["years_of_birth"]
